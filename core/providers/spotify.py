54341d51c97b8d9017c0cc12675a89cde546fa99
Dmitry Sus <dmitrysuswork@gmail.com>
2019-03-18 11:39:12 +0200

    added spotify autorization

--- core/providers/spotify.py
+++ core/providers/spotify.py
@@ -1,14 +1,35 @@
+import base64
 import os
 
 import requests
 from core.providers.base import MusicProvider
 
-SPOTIFY_API_TOKEN = os.environ.get('SPOTIFY_API_TOKEN')
+SPOTIFY_API_TOKEN = None
 
+SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
 class Spotify(MusicProvider):
     NAME = 'Spotify'
     _MUSIC_URL = 'https://open.spotify.com/track/{}'
+
+
+    def get_access_token(self):
+        api_url = 'https://accounts.spotify.com/api/token'
+
+        auth_str = bytes('{}:{}'.format(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET), 'utf-8')
+        b64_auth_str = base64.b64encode(auth_str).decode('utf-8')
+        headers = {
+                'Authorization': f'Basic {b64_auth_str}',
+            }
+
+        resp = requests.post(
+            url=api_url,
+            headers=headers,
+            data={"grant_type": "client_credentials"}
+        )
+        SPOTIFY_API_TOKEN = resp.json()['access_token']
+        return SPOTIFY_API_TOKEN
 
     def get_music_name(self, url):
         api_url = 'https://api.spotify.com/v1/tracks/{}'
@@ -30,7 +51,7 @@ class Spotify(MusicProvider):
         resp.raise_for_status()
 
         data = resp.json()
-        track_id = data['tracks']["items"][0]['id']
+        track_id = data['tracks']['items'][0]['id']
         url = self._MUSIC_URL.format(track_id)
         return url
 
@@ -38,11 +59,17 @@ class Spotify(MusicProvider):
         id_search = url.split('/')[-1]
         return id_search
 
-    @staticmethod
-    def get_headers():
+    def get_headers(self):
+
+        if not SPOTIFY_API_TOKEN:
+            return {
+                "Authorization": f'Bearer {self.get_access_token()}'
+            }
+
         return {
-            "Authorization": "Bearer {}".format(SPOTIFY_API_TOKEN)
-        }
+                "Authorization": f'Bearer {SPOTIFY_API_TOKEN}'
+            }
+
 
     @classmethod
     def is_music_url(self, url):
