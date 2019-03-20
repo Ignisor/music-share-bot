d9e4bc0e2c796f6ceee16a808f379cbbb2e37f78
dmitrysus <dmitrysuswork@gmail.com>
2019-03-20 11:45:15 +0200

    added Apple Music provider

--- core/providers/__init__.py
+++ core/providers/__init__.py
@@ -1,6 +1,7 @@
 from core.providers.spotify import Spotify
 from core.providers.youtube import YouTube
 from core.providers.youtube_music import YouTubeMusic
+from core.providers.apple_music import AppleMusic
 
 
-ALL_PROVIDERS = {YouTube, YouTubeMusic, Spotify}
+ALL_PROVIDERS = {YouTube, YouTubeMusic, Spotify, AppleMusic}
