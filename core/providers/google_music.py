feea62789d63991e4bf13a50479c6e91a95d1254
alex_shavelev <alexshavelev44@gmail.com>
2019-03-27 10:44:56 +0200

    fix g-music not song link

--- core/providers/google_music.py
+++ core/providers/google_music.py
@@ -21,6 +21,7 @@ class GoogleMusic(MusicProvider):
     def get_music_name(self, url):
         g_music_page = requests.get(url)
         soup = BeautifulSoup(g_music_page.content, 'html.parser')
-        title = soup.find('div', class_='title fade-out').text
-        artist = soup.find('div', class_='album-artist fade-out').text
-        return f'{artist} - {title}'
+        title = soup.find('div', class_='title fade-out')
+        artist = soup.find('div', class_='album-artist fade-out')
+        if title and artist:
+            return f'{artist.text} - {title.text}'
