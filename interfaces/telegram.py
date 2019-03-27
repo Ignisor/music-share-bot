39bbc4849c47696e8a50324d8ad9c91dceda29f4
alex_shavelev <alexshavelev44@gmail.com>
2019-03-27 16:22:42 +0200

    report button - https://github.com/Ignisor/music-share-bot/issues/22

--- interfaces/telegram.py
+++ interfaces/telegram.py
@@ -1,7 +1,7 @@
 import os
 
-from telegram import Bot, Update, MessageEntity
-from telegram.ext import Dispatcher, MessageHandler, Filters, Updater
+from telegram import Bot, Update, MessageEntity, InlineKeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup
+from telegram.ext import Dispatcher, MessageHandler, Filters, Updater, CallbackQueryHandler
 
 from core import process_message
 from interfaces.base import BotInterface
@@ -9,6 +9,7 @@ from interfaces.base import BotInterface
 
 class TelegramInterface(BotInterface):
     API_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+    ADMINS_CHAT = os.environ.get('BOT_ADMINS_CHAT')
 
     def __init__(self):
         self.bot = Bot(self.API_TOKEN)
@@ -20,6 +21,10 @@ class TelegramInterface(BotInterface):
             MessageHandler(
                 Filters.text & (Filters.entity(MessageEntity.URL) | Filters.entity(MessageEntity.TEXT_LINK)),
                 self._handle_message
+            ),
+
+            CallbackQueryHandler(
+                self._handle_mismatch_button
             )
         ]
 
@@ -30,17 +35,46 @@ class TelegramInterface(BotInterface):
         text = update.message.text
         response = process_message(text)
 
+        response_keyboard = TelegramInterface.get_keyboard(update.message)
+
         if response:
             update.message.reply_markdown(
                 response,
                 quote=True,
                 disable_web_page_preview=True,
                 disable_notification=True,
+                reply_markup=response_keyboard
             )
+
+    def _handle_mismatch_button(self, bot, update):
+        if TelegramInterface.ADMINS_CHAT:
+            user_message_id = update.callback_query.message.reply_to_message.message_id
+            bad_response_message_id = update.callback_query.message.message_id
+            message_chat_id = update.callback_query.message.chat.id
+
+            bot.forwardMessage(chat_id=TelegramInterface.ADMINS_CHAT, from_chat_id=message_chat_id,
+                               message_id=user_message_id)
+            bot.forwardMessage(chat_id=TelegramInterface.ADMINS_CHAT, from_chat_id=message_chat_id,
+                               message_id=bad_response_message_id)
+
+            bot.edit_message_reply_markup(chat_id=message_chat_id,
+                                  message_id=bad_response_message_id,
+                                  reply_markup=None)
+
 
     def process_message(self, message_data):
         update = Update.de_json(message_data, self.bot)
         self.dispatcher.process_update(update)
+
+    @staticmethod
+    def get_keyboard(message):
+        if TelegramInterface.ADMINS_CHAT:
+            message_id = message.message_id
+            feedback_button = InlineKeyboardButton(text="report mismatch",
+                                                   callback_data='/bad_response {}'.format(message_id))
+            buttons = [[feedback_button]]
+            return InlineKeyboardMarkup(inline_keyboard=buttons)
+
 
 
 class TelegramUpdaterInterface(TelegramInterface):
